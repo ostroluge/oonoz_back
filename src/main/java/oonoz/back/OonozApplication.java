9f7684311738685f2eba503599f1e5ce0994f728
Julien Flamen <flamen.julien@gmail.com>
2016-09-22 23:31:12 +0200

    Init projet

---
+++ src/main/java/oonoz/back/OonozApplication.java
@@ -0,0 +1,12 @@
+package oonoz.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OonozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OonozApplication.class, args);
+	}
+}
