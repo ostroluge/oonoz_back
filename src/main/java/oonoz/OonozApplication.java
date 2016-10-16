a14c66dcb4c466c9b2b3d85a48d758cd6c41ece9
Flamen Julien <flamen.julien@gmail.com>
2016-10-16 20:48:08 +0200

    Ajout config bdd prod

--- src/main/java/oonoz/OonozApplication.java
+++ src/main/java/oonoz/OonozApplication.java
@@ -2,7 +2,7 @@ package oonoz;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -10,15 +10,24 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import oonoz.domain.Supplier;
-import oonoz.repository.SupplierRepository;
-
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 public class OonozApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(OonozApplication.class);
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driverClassName;
+
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
+
+	@Value("${spring.datasource.username}")
+	private String dataSourceUsername;
+
+	@Value("${spring.datasource.password}")
+	private String dataSourcePassword;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(OonozApplication.class, args);
@@ -28,10 +37,10 @@ public class OonozApplication {
 	 @Bean(name = "dataSource")
 	    public DriverManagerDataSource dataSource() {
 	        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-	        driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-	        driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/oonoz");
-	        driverManagerDataSource.setUsername("postgres");
-	        driverManagerDataSource.setPassword("admin");
+	        driverManagerDataSource.setDriverClassName(driverClassName);
+	        driverManagerDataSource.setUrl(dataSourceUrl);
+	        driverManagerDataSource.setUsername(dataSourceUsername);
+	        driverManagerDataSource.setPassword(dataSourcePassword);
 	        return driverManagerDataSource;
 	    }
 	 
