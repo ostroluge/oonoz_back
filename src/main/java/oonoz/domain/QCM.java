b04250c17c5d70c0185dcc1d49cdc4ec9f921daf
vincent <jalzuritro@gmail.com>
2016-12-03 17:44:16 +0100

    Add Comment QCM

--- src/main/java/oonoz/domain/QCM.java
+++ src/main/java/oonoz/domain/QCM.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -193,6 +192,11 @@ public class QCM {
 		this.idSupplier = idSupplier;
 	}
 
+	/**
+	 * Gets the sub themes.
+	 *
+	 * @return the sub themes
+	 */
 	public List<SubTheme> getSubThemes() {
 		return subThemes;
 	}
