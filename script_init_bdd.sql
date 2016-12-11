5ea652cbf00b639dbbf8e2d6dc777a92561835fd
vincent <jalzuritro@gmail.com>
2016-12-11 17:18:06 +0100

    Change supplier (Ching Chong) valid  statut to false for the demonstratirion. (Get a supplier request)

--- script_init_bdd.sql
+++ script_init_bdd.sql
@@ -121,7 +121,7 @@ INSERT INTO player (id, firstname, lastname, mail, birthdate, is_active, usernam
 
 
 INSERT INTO supplier (id_player, is_valid, is_private_individual) values (104, TRUE, TRUE);
-INSERT INTO supplier (id_player, is_valid, is_private_individual, company_name, company_address, siret_number) values (105, TRUE, FALSE, 'Au pavillon des délices', '12 rue Victor Yugo, 75005 Chinatown','12345678912355');
+INSERT INTO supplier (id_player, is_valid, is_private_individual, company_name, company_address, siret_number) values (105, FALSE, FALSE, 'Au pavillon des délices', '12 rue Victor Yugo, 75005 Chinatown','12345678912355');
 
 INSERT INTO AUTHORITIES (id_authorities,username,role) values (101,'Jilief','ROLE_ADMIN');
 INSERT INTO AUTHORITIES (id_authorities,username,role) values (102,'Ostroluge','ROLE_ADMIN');
