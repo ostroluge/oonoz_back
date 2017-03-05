c795e775cef4ef2c30f8045bb89511642eaf8903
jilief <flamen.julien@gmail.com>
2017-03-05 18:36:29 +0100

    test multiple domain

--- src/main/java/oonoz/configuration/CorsFilter.java
+++ src/main/java/oonoz/configuration/CorsFilter.java
@@ -46,7 +46,8 @@ public class CorsFilter implements Filter {
         HttpServletResponse response=(HttpServletResponse) resp;
         HttpServletRequest request=(HttpServletRequest) req;
         String accessControlAllowHeaders = "Access-Control-Allow-Headers";
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342", "http://5.135.165.108");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:63342");
+        response.setHeader("Access-Control-Allow-Origin", "http://5.135.165.108");
         response.setHeader("Access-Control-Allow-Credentials","true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
